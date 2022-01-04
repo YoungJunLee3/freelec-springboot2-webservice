51d7f543090e6224a86b7e15e1ada0e46fa02709
“LeeYoungJun” <lee8307@gmail.com>
2022-01-04 11:10:16 +0900

    deploy 수정하기

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app/step2
+REPOSITORY=/home/ec2-user/app/step1
 PROJECT_NAME=freelec-springboot2-webservice
+
+cd $ REPOSITORY/$PROJECT_NAME/
+
+echo "> Git Pull"
+
+git pull
+
+echo "프로젝트build 시작"
+
+./gradlew build
+
+echo "step1 디렉토리로 이동"
+
+cd $REPOSITORY
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}*.jar)
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
@@ -23,9 +37,12 @@ fi
 
 echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep *.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
+
+nohup java -jar $REPOSITORY/$JAR_NAME 2>&1
+
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
